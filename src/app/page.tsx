0174cecec5472eb1ee124373e03df8a3e830720e
rohithgoud30 <rohithgoud30@gmail.com>
2025-04-02 00:21:03 -0400

    Feat(home): update home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Link from 'next/link'
-import { Button } from '@/components/ui/button'
 import HeroSection from '@/components/hero-section'
 
 export default function Home() {
   return (
     <div className='min-h-screen flex flex-col bg-white dark:bg-black'>
       <HeroSection />
-
-      <div className='flex justify-center pb-10'>
-        <Link href='/analyze'>
-          <Button className='bg-blue-600 hover:bg-blue-700 text-white'>
-            Analyze ToS or Privacy Policy
-          </Button>
-        </Link>
-      </div>
     </div>
   )
 }
