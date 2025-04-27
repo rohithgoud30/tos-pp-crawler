2b5797943e32ad8acca4cc4a4cbc70b654e221d5
rohithgoud30 <rohithgoud30@gmail.com>
2025-04-27 16:50:35 -0400

    Fix(navbar): add use client directive to fix server rendering error

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { FileText, Menu, User, LogOut } from 'lucide-react'
 import { ThemeToggle } from '@/components/ui/theme-toggle'
