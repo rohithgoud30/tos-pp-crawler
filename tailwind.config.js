151c9e3225ee232f73f0fa814159e09593e4f953
rohithgoud30 <rohithgoud30@gmail.com>
2025-04-27 03:24:23 -0400

    Fix(css): simplify Tailwind configuration and CSS styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  safelist: ['bg-transparent', 'bg-gray-50', 'shadow-sm'],
   theme: {
     container: {
       center: true,
